56071204bb3bb581ad0190106d1d08b185007c4e
Rt0x <144723959+Rt0x@users.noreply.github.com>
2024-05-23 00:44:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,281 +1,154 @@
-<?php
-header('Content-Type: text/html; charset=UTF-8');
-
-$db;
-
-include('database.php');
-
-session_start();
-  $log = !empty($_SESSION['login']);
-  
-function isp($value){
-  if(isset($value)) return $value;
-  return;
+* {
+    box-sizing: border-box;
 }
 
-function del_cook($cook, $vals = 0){
-  setcookie($cook.'_error', '', 100000);
-  if($vals) setcookie($cook.'_value', '', 100000);
+.tg {
+    display: flex;
+    flex-direction: column-reverse;
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  $name = (!empty($_COOKIE['name_error']) ? $_COOKIE['name_error'] : '');
-  $number = (!empty($_COOKIE['number_error']) ? $_COOKIE['number_error'] : '');
-  $email = (!empty($_COOKIE['email_error']) ? $_COOKIE['email_error'] : '');
-  $data = (!empty($_COOKIE['data_error']) ? strtotime($_COOKIE['data_error']) : '');
-  $radio = (!empty($_COOKIE['radio_error']) ? $_COOKIE['radio_error'] : '');
-  $lang = (!empty($_COOKIE['lang_error']) ? $_COOKIE['lang_error'] : '');
-  $biography = (!empty($_COOKIE['biography_error']) ? $_COOKIE['biography_error'] : '');
-  $check_mark = (!empty($_COOKIE['check_mark_error']) ? $_COOKIE['_error'] : '');
-
-  $errors = array();
-  $messages = array();
-  $values = array();
-  $error = true;
-
-function setVal($enName, $param){
-  global $values;
-  $values[$enName] = empty($param) ? '' : strip_tags($param);
+body {
+    background-image: url(4882808.jpg);
+    margin: 0;
+    background-size: cover;
+    background-position: center center;
+    background-attachment: fixed;
 }
 
-  function val_empty($enName, $val){
-    global $error, $errors, $values, $messages;
-    if($error) 
-    $error = empty($_COOKIE[$enName.'_error']);
-    $errors[$enName] = !empty($_COOKIE[$enName.'_error']);
-    $messages[$enName] = "<div class='messageError'>$val</div>";
-    $values[$enName] = empty($_COOKIE[$enName.'_value']) ? '' : $_COOKIE[$enName.'_value'];
-    del_cook($enName);
-    return;
-  }
-  
-
-
-  if (!empty($_COOKIE['save'])) {
-    setcookie('save', '', 100000);
-    setcookie('login', '', 100000);
-    setcookie('password', '', 100000);
-    $messages['success'] = 'Данные сохранены';
-    if (!empty($_COOKIE['password'])) {
-      $messages['info'] = sprintf('Вы можете <a href="login.php">войти</a> с логином <strong>%s</strong>
-        и паролем <strong>%s</strong> для изменения данных.',
-        strip_tags($_COOKIE['login']),
-        strip_tags($_COOKIE['password']));
-    }
-  }
-
-  val_empty("name", $name);
-  val_empty("number", $number);
-  val_empty("email", $email);
-  val_empty("data", $data);
-  val_empty("radio", $radio);
-  val_empty("lang", $lang);
-  val_empty("biography", $biography);
-  val_empty("check_mark",$check_mark);
-
-  $langsa = explode(',', $values['lang']);
-
-
-  if ($error && !empty($_SESSION['login'])) {
-    try {
-      $dbFD = $db->prepare("SELECT * FROM form_data WHERE user_id = ?");
-      $dbFD->execute([$_SESSION['user_id']]);
-      $fet = $dbFD->fetchAll(PDO::FETCH_ASSOC)[0];
-      $form_id = $fet['id'];
-      $_SESSION['form_id'] = $form_id;
-      $dbL = $db->prepare("SELECT l.name FROM form_data_lang f
-                            LEFT JOIN languages l ON l.id = f.id_lang
-                            WHERE f.id_form = ?");
-      $dbL->execute([$form_id]);
-      $langsa = [];
-      foreach($dbL->fetchAll(PDO::FETCH_ASSOC) as $item){
-        $langsa[] = $item['name'];
-      }
-      setVal('name', $fet['name']);
-      setVal('number', $fet['number']);
-      setVal('email', $fet['email']);
-      setVal('data', date("Y-m-d", $fet['data']));
-      setVal('radio', $fet['radio']);
-      setVal('lang', $lang);
-      setVal('biography', $fet['biography']);
-      setVal('check_mark', $fet['check_mark']);
-    }
-    catch(PDOException $e){
-      print('Error : ' . $e->getMessage());
-      exit();
-    }
-  }
-  include('form.php');
+form {
+    display: flex;
+    border-radius: 5px;
+    font-weight: normal;
+    border-collapse: collapse;
+    flex-flow: column wrap;
+    text-indent: inherit;
+    border-spacing: 2px;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-items: center;
+    color: #ffffff;
 }
-else{ 
-  $name = (!empty($_POST['name']) ? $_POST['name'] : '');
-  $number = (!empty($_POST['number']) ? $_POST['number'] : '');
-  $email = (!empty($_POST['email']) ? $_POST['email'] : '');
-  $data = (!empty($_POST['data']) ? $_POST['data'] : '');
-  $radio = (!empty($_POST['radio']) ? $_POST['radio'] : '');
-  $lang = (!empty($_POST['lang']) ? $_POST['lang'] : '');
-  $biography = (!empty($_POST['biography']) ? $_POST['biography'] : '');
-  $check_mark = (!empty($_POST['check_mark']) ? $_POST['check_mark'] : '');
-  $error = false;
 
-  if(isset($_POST['logout_form'])){
-    del_cook('name', 1);
-    del_cook('number', 1);
-    del_cook('email', 1);
-    del_cook('data', 1);
-    del_cook('radio', 1);
-    del_cook('lang', 1);
-    del_cook('biography', 1);
-    del_cook('radio', 1);
-    session_destroy();
-    header('Location: ./');
-    exit();
-  } 
-
-  $number1 = preg_replace('/\D/', '', $number);
-
-  function val_empty($cook, $comment, $usl){
-    global $error;
-    $res = false;
-    $setVal = $_POST[$cook];
-    if ($usl) {
-      setcookie($cook.'_error', $comment, time() + 24 * 60 * 60); //сохраняем на сутки
-      $error = true;
-      $res = true;
-    }
-    
-    if($cook == 'lang'){
-      global $lang;
-      $setVal = ($lang != '') ? implode(",", $lang) : '';
-    }
-    
-    setcookie($cook.'_value', $setVal, time() + 30 * 24 * 60 * 60); //сохраняем на месяц
-    return $res;
-  }
-  
-  if(!val_empty('name', 'Заполните поле', empty($name))){
-    if(!val_empty('name', 'Длина поля > 255 символов', strlen($name) > 255)){
-      val_empty('name', 'Поле не соответствует требованиям: <i>Фаимлмя Имя (Отчество)</i>, кириллицей', !preg_match('/^([а-яё]+-?[а-яё]+)( [а-яё]+-?[а-яё]+){1,2}$/Diu', $name));
-    }
-  }
-  if(!val_empty('number', 'Заполните поле', empty($number))){
-    if(!val_empty('number', 'Длина поля некорректна', strlen($number) != 11)){
-      val_empty('number', 'Поле должен содержать только цифры"', ($number != $number1));
-    }
-  }
-  if(!val_empty('email', 'Заполните поле', empty($email))){
-    if(!val_empty('email', 'Длина поля > 255 символов', strlen($email) > 255)){
-      val_empty('email', 'Поле не соответствует требованию example@mail.ru', !preg_match('/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/', $email));
-    }
-  }
-  if(!val_empty('data', "Выберите дату рождения", empty($data))){
-    val_empty('data', "Неверно введена дата рождения, дата больше настоящей", (strtotime('now') < strtotime($data)));
-  }
-  val_empty('radio', "Выберите пол", (empty($radio) || !preg_match('/^(m|f)$/', $radio)));
-  if(!val_empty('lang', "Выберите хотя бы один язык", empty($lang))){
-   
-    try {
-      $inQuery = implode(',', array_fill(0, count($lang), '?'));
-      $dbLangs = $db->prepare("SELECT id, name FROM languages WHERE name IN ($inQuery)");
-      foreach ($lang as $key => $value) {
-        $dbLangs->bindValue(($key+1), $value);
-      }
-      $dbLangs->execute();
-      $languages = $dbLangs->fetchAll(PDO::FETCH_ASSOC);
-    }
-    catch(PDOException $e){
-      print('Error : ' . $e->getMessage());
-      exit();
-    }
-    
-    val_empty('lang', 'Неверно выбраны языки', $dbLangs->rowCount() != count($lang));
-  }
-  if(!val_empty('biography', 'Заполните поле', empty($biography))){
-    val_empty('biography', 'Длина текста > 65 535 символов', strlen($biography) > 65535);
-  }
-  val_empty('check_mark', "Ознакомьтесь с контрактом", empty($check_mark));
-  
-  if ($error) {
-    // При наличии ошибок перезагружаем страницу и завершаем работу скрипта.
-    header('Location: index.php');
-    exit();
-  }
-  else {
-    del_cook('name');
-    del_cook('number');
-    del_cook('email');
-    del_cook('data');
-    del_cook('radio');
-    del_cook('lang');
-    del_cook('biography');
-    del_cook('check_mark');
-  }
-  
-
-
-   // Проверяем меняются ли ранее сохраненные данные или отправляются новые.
-   if ($log) { 
-      
-    $stmt = $db->prepare("UPDATE form_data SET name = ?, number = ?, email = ?, data = ?, radio = ?, biography = ? WHERE user_id = ?");
-    $stmt->execute([$name, $number, $email, strtotime($data), $radio, $biography, $_SESSION['user_id']]);
-    var_dump ($data);
-    print_r($db->errorInfo());
-
-    $stmt = $db->prepare("DELETE FROM form_data_lang WHERE id_form = ?");
-    $stmt->execute([$_SESSION['form_id']]);
-
-    $stmt1 = $db->prepare("INSERT INTO form_data_lang (id_form, id_lang) VALUES (?, ?)");
-    foreach($languages as $row){
-        $stmt1->execute([$_SESSION['form_id'], $row['id']]);
-    }
-    // TODO: перезаписать данные в БД новыми данными,
-    // кроме логина и пароля.
-  }
-  else {
-    echo '1';
-    // Генерируем уникальный логин и пароль.
-    // TODO: сделать механизм генерации, например функциями rand(), uniquid(), md5(), substr().
-    $login = substr(uniqid(), 0, 4).rand(10, 100);
-    $password = rand(100, 1000).substr(uniqid(), 4, 10);
-    // Сохраняем в Cookies.
-    setcookie('login', $login);
-    setcookie('password', $password);
-
-    
-
-
-  try {
-
-    $stmt=$db->prepare("INSERT INTO users(login, password) VALUES (?, ?)");
-    $stmt->execute([$login, md5($password) ]);
-
-
-    $fid = $db->lastInsertId();
-    $stmt = $db->prepare("INSERT INTO form_data (user_id, name, number, email, data, radio, biography) VALUES (?, ?, ?, ?, ?, ?, ?)");
-    $stmt->execute([$fid, $name, $number, $email, strtotime($data), $radio, $biography]);
-    $stmt1 = $db->prepare("INSERT INTO form_data_lang (id_form, id_lang) VALUES (?, ?)");
-    foreach($languages as $row){
-        $stmt1->execute([$fid, $row['id']]);
-    }
-  }
-  catch(PDOException $e){
-    print('Error : ' . $e->getMessage());
-    exit();
-  }
-  setcookie('name_value', $name, time() + 24 * 60 * 60 * 365);
-  setcookie('number_value', $number, time() + 24 * 60 * 60 * 365);
-  setcookie('email_value', $email, time() + 24 * 60 * 60 * 365);
-  setcookie('data_value', $data, time() + 24 * 60 * 60 * 365);
-  setcookie('radio_value', $radio, time() + 24 * 60 * 60 * 365);
-  setcookie('lang_value', implode(",", $lang), time() + 24 * 60 * 60 * 365);
-  setcookie('biography_value', $biography, time() + 24 * 60 * 60 * 365);
-  setcookie('check_mark_value', $check_mark, time() + 24 * 60 * 60 * 365);
+.custom-btn {
+    width: 150px;
+    height: 60px;
+    color: #fff;
+    border-radius: 30px;
+    padding: 10px 25px;
+    font-family: 'Lato', sans-serif;
+    font-weight: 500;
+    background: transparent;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    position: relative;
+    display: inline-block;
 }
-  // Сохраняем куку с признаком успешного сохранения.
-  setcookie('save', '1');
 
-  // Делаем перенаправление.
-  header('Location: index.php');
+.btn-1 {
+    background: #ffffff;
+    background: -webkit-radial-gradient(circle farthest-corner at center center, #000000 0%, #1f0a46 67%);
+    background: -moz-radial-gradient(circle farthest-corner at center center, #000000 0%, #1f0a46 67%);
+    background: radial-gradient(circle farthest-corner at center center, #000000 0%, #1f0a46 67%);
+    border: none;
+}
+
+h3#form.text-center {
+    color: #ffffff;
+}
+
+
+h5 {
+    text-align: center;
+    color: aliceblue;
+}
+
+a {
+    position: absolute;
+    text-align: center;
+    color: aliceblue;
+    font-weight: 900;
+}
+
+img {
+    width: 94%;
+    height: 94%;
+}
+
+div.wrap2.lh-lg.font-monospace {
+    position: absolute;
+    padding: 0px;
+    margin: 10px 610px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 100px rgb(30, 2, 66);
+    background-color: hsla(0, 0%, 0%, 0.192);
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-content: flex-start;
+    justify-content: center;
+    color: #fff;
+    align-items: stretch;
+}
+
+
+@media screen and (min-width: 415px) {
+    body {
+        background-image: url(4882808.jpg);
+        margin: 0;
+        background-size: cover;
+        background-position: center center;
+        background-attachment: fixed;
+    }
+
+
+
+    div.wrap1.lh-lg.font-monospace {
+        order: 1;
+        padding: 50px;
+        margin: 70px 450px;
+        width: 800px;
+        border-radius: 10px;
+
+        background-color: hsla(0, 0%, 0%, 0.192);
+    }
+
+
+    h5 {
+        text-align: center;
+        color: rgb(255, 255, 255);
+    }
+
+    a {
+        position: absolute;
+        text-align: center;
+        color: rgb(255, 255, 255);
+        font-weight: 900;
+    }
+
+    img {
+        width: 94%;
+        height: 94%;
+    }
+
+    div.wrap2.lh-lg.font-monospace {
+        position: absolute;
+        padding: 0px;
+        margin: 10px 610px;
+        border-radius: 10px;
+        box-shadow: 0px 0px 100px rgb(30, 2, 66);
+        background-color: hsla(0, 0%, 0%, 0.192);
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-content: flex-start;
+        justify-content: center;
+        align-items: stretch;
+        color: #fff;
+    }
+
+
+
+
+
 
 }
-?>
