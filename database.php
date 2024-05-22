bf6beaa5ef176497286adf975ce6180c837f9e9d
Rt0x <144723959+Rt0x@users.noreply.github.com>
2024-05-23 00:45:30 +0300

    Update database.php

--- database.php
+++ database.php
@@ -1,23 +1,4 @@
 <?php
-$user = 'u67307';
-$pass = '2532509';
-$db = new PDO(
-    'mysql:host=localhost;dbname=u67307',
-    $user,
-    $pass,
-    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-);
-
-try {
-    $stmt = $db->prepare("INSERT INTO programming_languages (language_name) VALUES (:language_name)");
-    $lange = $_POST['lange'];
-
-    $kl = implode($Languages);
-    $stmt->bindParam(':language_name', $kl);
-    $stmt->execute();
-
-} catch (PDOException $e) {
-    print ('Error : ' . $e->getMessage());
-    exit();
-}
+$db = new PDO('mysql:host=localhost;dbname=u67299', 'u67299', '3989934',
+     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 ?>
